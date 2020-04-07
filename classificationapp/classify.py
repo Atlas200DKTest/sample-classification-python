10d6812c2ab3f5924f7680af127df72cff387b68
ascendhuawei <ruochen.wen@huawei.com>
2020-04-07 12:05:32 -0700

    copy updated files from gitee to github

--- classificationapp/classify.py
+++ classificationapp/classify.py
@@ -17,13 +17,14 @@ dstFileDir = './resnet18Result/'
 
 def Resnet18PostProcess(resultList, srcFilePath, dstFilePath, fileName):
         if resultList is not None :
-			firstConfidence, firstClass = post_process.GenerateTopNClassifyResult(resultList, 1)
-			firstLabel = imageNetClasses.imageNet_classes[firstClass[0]]
-			dstFileName = os.path.join('%s%s' % (dstFilePath, fileName))
-			srcFileName = os.path.join('%s%s' % (srcFilePath, fileName))
-			image = cv.imread(srcFile)
-			cv.putText(image, text, (15,20), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.0, (0, 0, 255) )
-			cv.imwrite(dstFile, image)
+                firstConfidence, firstClass = post_process.GenerateTopNClassifyResult(resultList, 1)
+                firstLabel = imageNetClasses.imageNet_classes[firstClass[0]]
+                dstFileName = os.path.join('%s%s' % (dstFilePath, fileName))
+                srcFileName = os.path.join('%s%s' % (srcFilePath, fileName))
+                image = cv.imread(srcFileName)
+                txt = firstLabel + " " + str(round(firstConfidence[0]*100,2))
+                cv.putText(image, txt, (15,20), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.0, (0, 0, 255) )
+                cv.imwrite(dstFileName, image)
         else :
                 print('graph inference failed ')
                 return None
@@ -38,16 +39,14 @@ def main():
         dvppInWidth = 224
         dvppInHeight = 224
         start = time.time()
-        jpegHandler.mkdirown(dstFileDir)
+        if not os.path.exists(dstFileDir):
+                os.mkdir(dstFileDir)
         pathDir =  os.listdir(srcFileDir)
         for allDir in pathDir :
                 child = os.path.join('%s%s' % (srcFileDir, allDir))
-                if( not jpegHandler.is_img(child) ):
-                        print('[info] file : ' + child + ' is not image !')
-                        continue
                 input_image = cv.imread(child)
                 input_image = cv.resize(input_image, (dvppInWidth, dvppInHeight))
-                input_image = cv.cvtColor(input_image, cv.COLOR_BGR2YUV_I420)
+                # input_image = cv.cvtColor(input_image, cv.COLOR_BGR2YUV_I420)
                 resultList = myGraph.Inference(input_image)
                 if resultList is None :
                         print("graph inference failed")
